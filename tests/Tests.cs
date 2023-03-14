ed897600df1e258d9339f63930dcc35fa3fbed46
Bastian Kuolt <bk@incari.com>
2023-03-14 18:57:56 +0100

    Added unit test support

---
+++ tests/Tests.cs
@@ -0,0 +1,17 @@
+using System;  
+using Microsoft.VisualStudio.TestTools.UnitTesting;  
+  
+namespace bgl  
+{  
+    [TestClass]  
+    public class UnitTest1  
+    {  
+        [TestMethod]  
+        public void GetNameTest()  
+        {  
+       
+            Assert.AreEqual(true, true);  
+        }  
+  
+    }  
+}  
