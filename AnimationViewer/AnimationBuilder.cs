c5f9166440b1df2b8b30a13d1b79235959626e28
Bastian Kuolt <bk@incari.com>
2023-05-20 18:22:03 +0200

    WIP

--- AnimationViewer/AnimationBuilder.cs
+++ AnimationViewer/AnimationBuilder.cs
@@ -3,19 +3,18 @@ using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Processing;
 using SixLabors.ImageSharp.Metadata.Profiles.Iptc;
 
-using RGBA = SixLabors.ImageSharp.PixelFormats.Rgba32;
+using Pixel = SixLabors.ImageSharp.PixelFormats.Rgba32;
+using Image = SixLabors.ImageSharp.Image<SixLabors.ImageSharp.PixelFormats.Rgba32>;
 
 namespace bgl {
-
-    class AnimationBuilder {
-        public struct vec2 {
-            public vec2() {
-                x = 0;
-                y = 0;
-            }
-            public int x;
-            public int y;
+    public struct vec2 {
+        public vec2() {
+            x = 0;
+            y = 0;
         }
+        public int x;
+        public int y;
+    }
 
         public struct BoundingBox {
             public BoundingBox() {
@@ -46,6 +45,62 @@ namespace bgl {
              public vec2 max;
         }
 
+
+
+    class AnimationBuilder {
+
+        public Image BuildAnimation(Image[] images, string outputPath) {
+            // TODO: check whether images is emptx
+            // TODO: chekc if Path is valid
+                 
+            Image? resizedImage;
+            try
+            {
+                BoundingBox boundingBox = CalculateBoundingBox(images);
+                int width = boundingBox.GetWidth();
+                int height = boundingBox.GetHeight();
+
+                // create new multi-frame PNG image
+                int size = width * height;
+                resizedImage = new Image(width, height);
+                
+                // copy content for each image
+                for (int i = 0; i < images.Length; ++i) {
+                    var data = new Pixel[width * height];
+                    var frame = resizedImage.Frames.AddFrame(data);
+                    // TODO: copy from source image    
+
+                    var rectangle = new SixLabors.ImageSharp.Rectangle();
+                    images[i].Mutate( (image) => image.Crop(rectangle) );
+                    images.CopyTo(data, 0);
+                }
+
+            } catch {
+                // TODO: could not process images
+            }
+
+            try {
+                SetMetadata(resizedImage);
+                resizedImage.SaveAsPng(outputPath);
+            } catch {
+                // TODO: could not save image
+            }
+
+            // TODO: log savings
+            return resizedImage;
+        }
+
+        public void BuildAnimation(Image[] images) {
+            try {
+                string outputPath = "";  // TODO: prompt output path
+                BuildAnimation(images, outputPath);
+            } catch {
+                return; // TODO: show error dialog
+            }
+        }
+
+        /* ---------------------------------------------------------------------------------- */
+
         BoundingBox CalculateBoundingBox(BoundingBox a, BoundingBox c) {
             var boundingBox = new BoundingBox();
             // TODO
@@ -61,19 +116,19 @@ namespace bgl {
             return boundingBox;
         }
 
-        BoundingBox[] CalculateBoundingBoxes(SixLabors.ImageSharp.Image<RGBA>[] images) 
+        BoundingBox[] CalculateBoundingBoxes(Image[] images) 
         {
-            return new BoundingBox[0];
+            return new BoundingBox[0];  // TODO
         }
 
-        public BoundingBox CalculateBoundingBox(SixLabors.ImageSharp.Image<RGBA>[] images) 
+        public BoundingBox CalculateBoundingBox(Image[] images) 
         {
   
             BoundingBox[] boundingBoxes = CalculateBoundingBoxes(images);
             var boundingBox = CalculateBoundingBox(boundingBoxes);
             return boundingBox;
         }
-        public BoundingBox GetBoundingBox(SixLabors.ImageSharp.Image<RGBA> image) 
+        public BoundingBox GetBoundingBox(Image image) 
         {
             var boundingBox = new BoundingBox();
 
@@ -105,43 +160,23 @@ namespace bgl {
         //////////////////////////////////////////////////////////
         
         // TODO: makeStatic
-        public SixLabors.ImageSharp.Image<RGBA> BuildAnimation(SixLabors.ImageSharp.Image<RGBA>[] images, string outputPath) {
-            BoundingBox boundingBox = CalculateBoundingBox(images);
-            int width = boundingBox.GetWidth();
-            int height = boundingBox.GetHeight();
 
-            // create new multi-frame PNG image
-            int size = width * height;
-            var resizedImage = new SixLabors.ImageSharp.Image<RGBA>(width, height);
-            
-            // copy content for each image
-            for (int i = 0; i < images.Length; ++i) {
-                var data = new RGBA[width * height];
-                var frame = resizedImage.Frames.AddFrame(data);
-                // TODO: copy from source image    
-
-                var rectangle = new SixLabors.ImageSharp.Rectangle();
-                images[i].Mutate( (image) => image.Crop(rectangle) );
-                images.CopyTo(data, 0);
+        private void SetMetadata(Image? image) {
+            if (image == null) {
+                return;  // ther is nothing to do
             }
 
-            resizedImage.SaveAsPng(outputPath, null /* TODO: Create custom PNGDecoder */);
-
-            resizedImage.Metadata.IptcProfile = new SixLabors.ImageSharp.Metadata.Profiles.Iptc.IptcProfile();
-
+            image.Metadata.IptcProfile = new SixLabors.ImageSharp.Metadata.Profiles.Iptc.IptcProfile();
             // TODO
-            resizedImage.Metadata.IptcProfile.SetValue(IptcTag.Name, "Pokemon");
-            resizedImage.Metadata.IptcProfile.SetValue(IptcTag.Byline, "Thimo Pedersen");
-            resizedImage.Metadata.IptcProfile.SetValue(IptcTag.Caption, "Classic Pokeball Toy on a bunch of Pokemon Cards. Zapdos, Ninetales and a Trainercard visible.");
-            resizedImage.Metadata.IptcProfile.SetValue(IptcTag.Source, @"https://rb.gy/hgkqhy");
-            resizedImage.Metadata.IptcProfile.SetValue(IptcTag.Keywords, "Pokemon");
-            resizedImage.Metadata.IptcProfile.SetValue(IptcTag.Keywords, "Pokeball");
-            resizedImage.Metadata.IptcProfile.SetValue(IptcTag.Keywords, "Cards");
-            resizedImage.Metadata.IptcProfile.SetValue(IptcTag.Keywords, "Zapdos");
-            resizedImage.Metadata.IptcProfile.SetValue(IptcTag.Keywords, "Ninetails");
-
-            // TODO: log savings
-            return resizedImage;
+            image.Metadata.IptcProfile.SetValue(IptcTag.Name, "Pokemon");
+            image.Metadata.IptcProfile.SetValue(IptcTag.Byline, "Thimo Pedersen");
+            image.Metadata.IptcProfile.SetValue(IptcTag.Caption, "Classic Pokeball Toy on a bunch of Pokemon Cards. Zapdos, Ninetales and a Trainercard visible.");
+            image.Metadata.IptcProfile.SetValue(IptcTag.Source, @"https://rb.gy/hgkqhy");
+            image.Metadata.IptcProfile.SetValue(IptcTag.Keywords, "Pokemon");
+            image.Metadata.IptcProfile.SetValue(IptcTag.Keywords, "Pokeball");
+            image.Metadata.IptcProfile.SetValue(IptcTag.Keywords, "Cards");
+            image.Metadata.IptcProfile.SetValue(IptcTag.Keywords, "Zapdos");
+            image.Metadata.IptcProfile.SetValue(IptcTag.Keywords, "Ninetails");
         }
     }
 }
